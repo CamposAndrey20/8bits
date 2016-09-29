61d465678d10f05f967b0fdd50c35a6f19bc3b1b
Omar Segura <omarseguvi@gmail.com>
2016-09-29 09:35:25 -0600

    Se agrego la excepcion para cuando un numero es mas grande que 255

--- src/js/JSNum.java
+++ src/js/JSNum.java
@@ -1,8 +1,11 @@
 package eightBit.js;
 
 public class JSNum extends JSAtom<Double>{
+  final int max = 255;
    public JSNum(Double value){
       super(value);
+      if(value > max)
+        throw new RuntimeException("Error de semantica -> Un numero no puede ser mas grande que 255");
    }
-   
+
 }
